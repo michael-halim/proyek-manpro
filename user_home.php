3f9063294f37b8b80cd89fae3f671618d98d3419
dimas-renanda <dimasrenanda@gmail.com>
2022-04-13 21:46:48 +0700

    session nama,split navbar

--- user_home.php
+++ user_home.php
@@ -1,3 +1,17 @@
+<?php
+include 'connect.php';
+if (!isset($_SESSION['email'])) {
+	// if ($_SESSION['email'] == "admin@gmail.com") {
+	// 	header('location: mobile_admin_home.php');
+	// } else {
+		header("Location: login.php");
+	// }
+
+	// echo $_SESSION['email'];
+	// echo date("Y-m-d H:i:s");
+	
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,29 +33,7 @@
   <header role="banner" class="probootstrap-header">
     <div class="container-fluid">
       <!-- <div class="row"> -->
-        <a href="index.html" class="probootstrap-logo">Baca Alkitab<span>.</span></a>
-        
-        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
-        <div class="mobile-menu-overlay"></div>
-
-        <nav role="navigation" class="probootstrap-nav hidden-xs">
-          <ul class="probootstrap-main-nav">
-            <li><a href="user_home.php">Home</a></li>
-            <li><a href="material.php">Materi</a></li>
-            <li><a href="group.php">Grup</a></li>
-            <li class="probootstrap-cta"><a href="signup.html">Hi , nama</a></li>
-          </ul>
-          <div class="extra-text visible-xs">
-            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
-            <h5>Social</h5>
-            <ul class="social-buttons">
-              <li><a href="#"><i class="icon-twitter"></i></a></li>
-              <li><a href="#"><i class="icon-facebook"></i></a></li>
-              <li><a href="#"><i class="icon-instagram2"></i></a></li>
-            </ul>
-            <p><small>&copy; Kelompok 11.2021.</small></p>
-          </div>
-        </nav>
+        <?php require_once('user_navbar.php'); ?>
 
         <section class="probootstrap-intro">
           <div class="container">
---
+++ user_navbar.php
@@ -0,0 +1,28 @@
+
+
+
+<a href="index.html" class="probootstrap-logo">Baca Alkitab<span>.</span></a>
+        
+        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <div class="mobile-menu-overlay"></div>
+
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+          <ul class="probootstrap-main-nav">
+            <li><a href="user_home.php">Home</a></li>
+            <li><a href="material.php">Materi</a></li>
+            <li><a href="group.php">Grup</a></li>
+            <li class="probootstrap-cta"><a href="signup.html">Hi, <?= $_SESSION['nama']; ?></a> </li>
+          </ul>
+          <div class="extra-text visible-xs">
+            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
+            <h5>Social</h5>
+            <ul class="social-buttons">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook"></i></a></li>
+              <li><a href="#"><i class="icon-instagram2"></i></a></li>
+            </ul>
+            <p><small>&copy; Kelompok 11.2021.</small></p>
+          </div>
+        </nav>
+
+ 
