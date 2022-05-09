032c4aa49970efe2f25faa39b25e6f0d82e05df9
marcellasharon <marcellasharon31@gmail.com>
2022-05-09 21:52:03 +0700

    admin home

--- admin_home.php
+++ admin_home.php
@@ -57,7 +57,7 @@ if(isset($_POST['submit'])) {
   <link rel="stylesheet" type="text/css" href="style.css">
 
 	<script src="assets/js/admin_home.js"></script>
-	<script src="assets/js/admin_sidebar.js"></script>
+	<!-- <script src="assets/js/admin_sidebar.js"></script> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script>
 
