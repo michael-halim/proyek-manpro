7235e92abddde047a97b8dea59af19e85fdc6b98
marcellasharon <marcellasharon31@gmail.com>
2022-05-31 09:06:40 +0700

    ui sisi user list group member

--- group.php
+++ group.php
@@ -34,6 +34,7 @@ include 'connect.php';
       background-color: #464646;
       width: 100%;
       height: auto;
+      font-family: 'Roboto', sans-serif;
     }
 
     .responsive {
@@ -41,11 +42,51 @@ include 'connect.php';
       height: auto;
     }
 
-    table,
+    /* table,
     th,
     td {
       border: 2px solid black;
-    }
+    } */
+    table{
+  width:100%;
+  table-layout: fixed;
+}
+.tbl-header{
+  background-color: rgba(255,255,255,0.3);
+ }
+.tbl-content{
+  /* height:0px; */
+  overflow-x:auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255,255,255,0.3);
+}
+th{
+  padding: 20px 15px;
+  text-align: left;
+  font-weight: 500;
+  font-size: 18px;
+  color: #000;
+  text-transform: uppercase;
+}
+td{
+  padding: 15px;
+  text-align: left;
+  vertical-align:middle;
+  font-weight: 300;
+  font-size: 15px;
+  color: #000;
+  border-bottom: solid 1px rgba(255,255,255,0.1);
+}
+
+::-webkit-scrollbar {
+    width: 6px;
+} 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+} 
+::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+}
   </style>
 </head>
 
@@ -87,15 +128,27 @@ include 'connect.php';
         <h2>Anggota</h2>
       </div>
   </div>';
-            echo '<div class="asd">';
-            echo ' <div >
-  <table style="background-color:whitesmoke;width:100%" class="table">
-    <tr>
-      <th>Nama</th>
-      <th>Email</th>
-      <th>Hp</th>
-      <th>Status</th>
-    </tr>';
+            // echo '<div class="asd">';
+  //           echo ' <div >
+  // <table style="background-color:whitesmoke;width:100%" class="table">
+  //   <tr>
+  //     <th>Nama</th>
+  //     <th>Email</th>
+  //     <th>Hp</th>
+  //     <th>Status</th>
+  //   </tr>';
+  echo '<div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>Nama</th>
+          <th>Email</th>
+          <th>Hp</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+    </table>
+  </div>';
             //<?php
             $emailnya = $_SESSION["email"];
             $sql = "SELECT DISTINCT nama,email,hp,ketua 
@@ -123,18 +176,33 @@ include 'connect.php';
               $email = $row["email"];
               $hp = $row["hp"];
               $ketua = $row["ketua"];
-              echo "<tr>";
-              echo "<th>$nama</th>";
-              echo "<th>$email</th>";
-              echo "<th>$hp</th>";
-              if ($ketua == 1) {
-                echo "<th>Ketua</th>";
-              }
-              if ($ketua == 0) {
-                echo "<th>Anggota</th>";
-              }
-              echo "</tr>";
+              echo '<div class="tbl-content">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+        <tr>';
+          echo "<td>$nama</td>";
+          echo "<td>$email</td>";
+          echo "<td>$hp</td>";
+          if ($ketua == 1) {
+            echo '<td>Ketua</td>';
+          }
+          if ($ketua == 0) {
+            echo '<td>Anggota</td>';
+          }
+          echo '</tr>';
+              // echo "<tr>";
+              // echo "<th>$nama</th>";
+              // echo "<th>$email</th>";
+              // echo "<th>$hp</th>";
+              // if ($ketua == 1) {
+              //   echo "<th>Ketua</th>";
+              // }
+              // if ($ketua == 0) {
+              //   echo "<th>Anggota</th>";
+              // }
+              // echo "</tr>";
             }
+            echo '</tbody>';
             echo '</table>';
             echo '</div>';
           }
